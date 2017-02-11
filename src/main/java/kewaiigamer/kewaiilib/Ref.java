bfea07a67289bd4ef3e23fa27509f6bbd912368d
KewaiiGamer <miguelviegas1999@gmail.com>
2017-02-11 16:20:32 +0000

    Fixed version number

--- src/main/java/kewaiigamer/kewaiilib/Ref.java
+++ src/main/java/kewaiigamer/kewaiilib/Ref.java
@@ -3,6 +3,6 @@ package kewaiigamer.kewaiilib;
 public class Ref {
     public static final String MODID = "kelib";
     public static final String NAME = "KewaiiLib";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.0";
     public static final String ACCEPTED_VERSIONS = "[1.10.2]";
 }
